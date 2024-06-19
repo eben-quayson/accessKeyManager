f8bf7bcc320f0223fd85066d97bd8c3c90de13c0
Ebenezer Quayson <quaysonebenezerawudzi@gmail.com>
2024-06-19 16:01:44 +0000

    importing cookie-session

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const session = require('express-session');
+const session = require('cookie-session');
 const path = require('path');
 const dotenv = require('dotenv');
 
